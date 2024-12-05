ff1e82ef0b2609b5c3a0ad3937b5aafdb405a9ae
Albert-Valdemora <albertvaldemorat@gmail.com>
2024-12-05 13:50:12 -0400

    Configurando la navegacion

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,49 @@
+
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import { AuthRouter } from './AuthRouter'
+// import { PrivateRoute } from './PrivateRoute';
+// import { PublicRoute } from './PublicRoute';
+import { PagoTrackerScreen } from "../components/screens/PagoTrackerScreen";
+
+
+
+export const AppRouter = () => {
+
+  
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+{/* 
+        <Route path='/auth/*' element={
+          <PublicRoute>
+            <AuthRouter />
+          </PublicRoute>
+          } />
+           */}
+
+        <Route path='/auth/*' element={
+         
+            <AuthRouter />
+         
+          } />
+
+        <Route path="/"  element={
+      
+            <PagoTrackerScreen />
+       
+          } />
+
+        {/* <Route path="/"  element={
+          <PrivateRoute>
+            <PagoTrackerScreen />
+          </PrivateRoute>
+          } /> */}
+
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+
+export const PublicRoute = ({children}) => {
+
+  const { uid } =  useSelector(state => state.auth)
+
+
+  return !uid
+    ? children
+    : <Navigate to="/" />;
+}
